a98d2fd988cf769211830ae9d1c8a992d21754a0
Derbeli Mohamed Dhia <68518867+DhiaDerbeli@users.noreply.github.com>
2021-08-31 22:24:08 +0100

    Add files via upload

---
+++ dsu.cpp
@@ -0,0 +1,28 @@
+struct dsu
+   {
+      vector<int> root, size;
+
+      dsu(int n)
+      {
+         size.resize(n + 1, 1);
+         root.resize(n + 1);
+         for (int i = 1; i <= n; i++) root[i] = i;
+      }
+
+      int find(int u) { return root[u] == u ? u : root[u] = find(root[u]); }
+
+      bool same(int u, int v)
+      {
+
+         if (find(u) == find(v)) return true;
+         return false;
+      }
+
+      void join(int u, int v){
+         u = find(u);
+         v = find(v);
+         if (size[u] < size[v]) swap(u, v);
+         root[v] = u;
+         size[u] += size[v];
+      }
+   };
