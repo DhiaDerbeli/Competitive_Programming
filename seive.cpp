149972b25da5226ab413afb7d703240813dce8df
Derbeli Mohamed Dhia <68518867+DhiaDerbeli@users.noreply.github.com>
2021-12-04 17:23:20 +0100

    Update seive.cpp

--- seive.cpp
+++ seive.cpp
@@ -1,51 +1,19 @@
-#include <bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace std;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-typedef long long ll;
-typedef long double ld;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
 
-#define pb push_back
-#define READ(t,n) for(ll i=0;i<n;i++) cin>>t[i];
-#define PRINT(t) for(auto i:t) cout<<i<<" ";cout<<endl;
-#define debug(x) cerr << #x << " is " << x << "\n"
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define endl "\n"
-#define REP(i,n) for(int i=0;i<n;i++)
-#define MOD 1000000007
 
-vector<bool> prime ;
+const int N = 1e6 + 5;
+bool prime[N+1] ;
  
-void seive(int n)
+void seive()
 {
+    for(int i=0; i<=N; i++) prime[i] = 1;
     prime[0] = 0;
     prime[1] = 0;
-    for (int p = 2; p * p <= n; p++)
+    for (int p = 2; p * p <= N; p++)
     {
         if (prime[p] == true)
         {
-            for (int i = p * p; i <= n; i += p)
+            for (int i = p * p; i <= N; i += p)
                 prime[i] = false;
         }
     }
-    //for (int p = 2; p <= n; p++)
-        //if (prime[p])
-            //cout << p << " ";
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-    prime.resize(n+1, true);
-    seive(n);
-    return 0;
 }
