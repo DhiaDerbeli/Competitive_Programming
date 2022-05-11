6abdc68f252abfafc2511abd8cf519bb4a067468
Derbeli Mohamed Dhia <68518867+DhiaDerbeli@users.noreply.github.com>
2022-05-11 02:07:44 +0200

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,33 +1,3 @@
-#include <bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace std;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-typedef long long ll;
-typedef long double ld;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-
-
-#define pb push_back
-#define READ(t,n) for(ll i=0;i<n;i++) cin>>t[i];
-#define PRINT(t) for(auto i:t) cout<<i<<" ";cout<<endl;
-#define debug(x) cerr << #x << " is " << x << "\n"
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define endl "\n"
-
-const ll INF=1e12;
-vector<vector<pair<ll , int>>> g ;
-vector<int> p ;
-vector<ll> d ;
-priority_queue < pair<ll,int> , vector< pair<ll,int> > , greater< pair<ll,int> > > pq;
- 
- 
 vll seg_tree;
 vll a;
 int n, q;
@@ -61,20 +31,4 @@ void build()
         seg_tree[i + n] = a[i];
     for (int i = n - 1; i >= 0; i--)
         seg_tree[i] = seg_tree[i * 2] + seg_tree[i * 2 + 1];
-}
- 
-int main()
-{
-    cin >> n >> q;
-    build();
-    while (q--)
-    {
-        int l, r, v;
-        //cin >> v;
-        cin >> l >> r;
-        l--;
-        r--;
-        //update(l,r,v);
-        cout << query(1, n - 1, 0, l, r) << endl;
-    }
 }
