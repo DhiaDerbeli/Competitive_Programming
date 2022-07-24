91a74e98a5decbb19ce2763c0bb0ab1fabfeac52
Derbeli Mohamed Dhia <68518867+DhiaDerbeli@users.noreply.github.com>
2022-07-24 12:59:15 +0100

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -9,7 +9,7 @@ ll query(int left, int right, int root = 1, int high = n-1, int low = 0)
     if (low >= left && high <= right)
         return seg_tree[root];
     int middle = (high + low) / 2;
-    return min(query(left, right, 2 * root, middle, low), query(left, right, 2 * root + 1, high, middle + 1));
+    return max(query(left, right, 2 * root, middle, low), query(left, right, 2 * root + 1, high, middle + 1));
 }
 
 void update(int pos, ll value){
@@ -24,13 +24,13 @@ void update(int pos, ll value){
 void build()
 {
     a.resize(n);
-    READ(a, n);
-    n = (1 << ((n&(n-1)) ? 32 : 31)- __builtin_clz(n));
+    for(int i=0; i<n; i++) cin >> a[i];
+    n = (1 << (32 - __builtin_clz(n)));
     seg_tree.resize(2 * n);
     for (int i = 0; i < a.size(); i++)
         seg_tree[i + n] = a[i];
     for (int i = n - 1; i >= 0; i--)
-        seg_tree[i] = seg_tree[i * 2] + seg_tree[i * 2 + 1];
+        seg_tree[i] = max(seg_tree[i * 2], seg_tree[i * 2 + 1]);
 }
 
 //query(l-1, r-1)
